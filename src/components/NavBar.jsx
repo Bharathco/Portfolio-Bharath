448c70e365e978729d9cf1a341b11e7efbfc4b5a
bharath.rajashekar3@gmail.com <bharath.rajashekar3@gmail.com>
2024-06-30 01:25:39 +0530

    First commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,71 @@
+
+import logo from "../assets/BR-logo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub  } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { BiDownload } from "react-icons/bi";
+import { useState } from "react";
+const NavBar = () => {
+  
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleHover = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
+  return (
+      <nav className="mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center ">
+            <img className="mx-2 w-10" src={logo} alt="" />
+        </div>
+       
+        <div className="m-8 flex items-center justify-centergap-4 text-2xl gap-5">
+        <a
+          href="https://www.linkedin.com/in/bharath-rajashekar-69b2591a7"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:text-blue-700"
+        ><FaLinkedin/></a>
+
+<a
+          href="https://github.com/Bharathco"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-100 hover:text-gray-400"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/bharath_raj_naik_/."
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-pink-500 hover:text-pink-700"
+        >
+          <FaInstagram />
+        </a>
+            
+        <a
+          href="https://drive.google.com/file/d/1Xp5Hfk-10h-XbKx0QbtBsTOXDKjR8elN/view?usp=sharing"
+          download="Bharath_Rajashekar_Resume.pdf"
+          target="_blank"
+          className="relative text-green-500 hover:text-green-700"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleMouseLeave}
+        >
+          <BiDownload />
+          {isHovering && (
+              <span className="absolute  text-white text-xs px-1 py-1 top-full left-1/2 transform -translate-x-1/2 -translate-y-0.5">
+              Resume
+            </span>
+          )}
+        </a>
+        </div>
+      </nav>
+  );
+}
+
+export default NavBar
---
+++ src/constants/index.js
@@ -0,0 +1,74 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With hands-on experience from an on-site Internship, I have honed my skills in front-end technologies like HTML, CSS, Javascript, TailwindCSS and ReactJS as well as working on back-end technologies like NodeJs, ExpressJs, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am Bharath Rajashekar, a Computer Science and Design undergraduate, currently in my Pre-final year at DSATM, graduating in 2025. I have a strong foundation in web development, having worked as a Web Developer Intern at Art of Living Digital. My expertise lies in creating responsive and dynamic web applications using React.js, along with a solid understanding of Data Structures and Algorithms (DSA).\n\nIn addition to my web development skills, I am proficient in Java, Operating Systems (OS), Database Management Systems (DBMS), Computer Networks and GenerativeAI. My diverse skill set and hands-on experience with both front-end and back-end technologies equip me to tackle complex technical challenges and contribute effectively to any development team.`;
+
+
+export const EXPERIENCES = [
+  {
+    year: "Nov 2023 - Dec 2023",
+    role: "React Developer Intern",
+    company: "Art of living Digital, a unit of Sumeru Software Solutions",
+    description: `contributed to the Art of Living Website for Latin American countries under the guidance of Project Manager Ms. Neeru Chaurasia. Integrating into a project the developers had been working on for months, my task was to create a pop-up modal using React.js, HTML, Tailwind CSS, and JavaScript for a seamless user experience. Working alongside developers provided insights into industry practices, enhancing my technical skills. I am grateful for this opportunity and eager to apply these experiences to future endeavors..`,
+    technologies: ["HTML", "CSS", "TrailwindCSS", "Javascript", "React.js"],
+  },
+  {
+    year: "Oct 2023 - Dec-2023",
+    role: "Web Developer Intern",
+    company: "Varcons Technologies Pvt Ltd",
+    description: `Designed and developed user interfaces for web applications  titled 'Web Template for Biometric Authentication Landing Page,' aimed at creating fully-fledged functional web templates for a client. Designed functional web pages and utilized databases to collect, store, and sort data, aligning with design briefs and client specifications provided in the project proposal.`,
+    technologies: ["HTML", "CSS", "Javascript", "Figma"],
+  },
+  {
+    year: "May 2023 - July 2023",
+    role: "Machine Learning Intern",
+    company: "LanguifyAI",
+    description: `worked on an engaging project focused on Object Detection using Machine Learning (ML) and Python. Under the guidance of the LanguifyAI team, I developed an object detection system, curated and prepared data for machine learning models, implemented and optimized ML models using Python and libraries like TensorFlow and PyTorch, conducted thorough testing and evaluation, and maintained detailed project documentation. This experience provided me with practical insights into ML, Python programming, and object detection, furthering my path towards a successful career in AI and ML..`,
+    technologies: ["Python", "Machine Learning", "TensorFlow",],
+  },
+ 
+];
+
+export const PROJECTS = [
+ 
+  {
+    title: "Landmark Detection Project",
+    image: project1,
+    description:
+      "project focuses on advanced object detection, specifically identifying and labeling place names within images. This technology enhances user experience by providing real-time, accurate recognition and annotation of locations in images, streamlining the extraction of place-related information from visual content. It finds applications in tourism, geolocation services, and more.",
+    technologies: ["Python", "Machine learning", "TensorFlow",],
+  },
+  {
+    title: "Data Visualization Project - Netflix Dataset",
+    image: project2,
+    description:
+      "project focused on the Netflix dataset, developing a comprehensive data dashboard that explores key aspects of the streaming platform. The dashboard highlights top content based on runtime and IMDB ratings, compares subscription costs, and delves into popular genres. This project showcases the power of data visualization in transforming raw information into actionable insights, empowering decision-making and enhancing user experiences. If you're interested in exploring the dashboard or discussing data visualization.",
+    technologies: ["Tableau", "Data Visualization", "Data Analysis",],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A responsive personal portfolio website showcasing experience, projects, skills, and contact information.",
+    technologies: ["HTML", "TailwindCSS", "React", "JavaScript", "React-icons", "Framer motion"
+    ],
+  },
+  
+  // {
+  //   title: "Blogging Platform",
+  //   image: project4,
+  //   description:
+  //     "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+  //   technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  // },
+];
+
+export const CONTACT = {
+  address: "Bangalore, India",
+  phoneNo: "+91-9686698498 ",
+  email: "bharath.rajashekar3@gmail.com",
+};
